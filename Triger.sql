13e9d1fee5a2f8292c9c24bb4be3ecc299cb2d0e
Kuplyaev Aleksey <a.kuplyaev@gmail.com>
2016-05-26 17:19:02 +0300

    Создание тригера

--- Triger.sql
+++ Triger.sql
@@ -2,13 +2,13 @@ use KuplyaevIntensive
 GO
 --ALTER TABLE  Tasks ADD date_modification date
 GO 
-CREATE TRIGGER any_change 
+ALTER TRIGGER any_change 
 ON dbo.Tasks 
 AFTER UPDATE
 AS if exists (select  * from deleted)
 begin 
 declare @id_update int
-set @id_update=(Select id_task from Tasks)
+set @id_update=(Select id_task from deleted)
 update Tasks set date_modification=GETDATE()
 where id_task=@id_update
 end 
