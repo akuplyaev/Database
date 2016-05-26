25000aad2425fff32a40aaa77515a1a5fb491458
Kuplyaev Aleksey <a.kuplyaev@gmail.com>
2016-05-26 17:19:02 +0300

    Создание тригера

--- Triger.sql
+++ Triger.sql
@@ -8,7 +8,7 @@ AFTER UPDATE
 AS if exists (select  * from deleted)
 begin 
 declare @id_update int
-set @id_update=(Select id_task from Tasks)
+set @id_update=(Select id_task from deleted)
 update Tasks set date_modification=GETDATE()
 where id_task=@id_update
 end 
