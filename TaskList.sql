dea982a247ea79aeead8c2e7e312ca0e62cde7e8
Kuplyaev Aleksey <a.kuplyaev@gmail.com>
2016-05-26 14:28:21 +0300

    fix

--- TaskList.sql
+++ TaskList.sql
@@ -5,14 +5,16 @@ IF (NOT EXISTS (Select name_project from Projects where name_project='Спискок за
 		INSERT INTO Projects
 			values ('Список задач')	
 			WHILE (select COUNT(*) from Projects WHERE name_project not in (Select name_task from Tasks))>0		
-			INSERT INTO Tasks 
-				values ((select name_task from Tasks
-						 WHERE name_task not in (Select name_project from Projects)
-						 ),
+			BEGIN
+				INSERT INTO Tasks 
+					values ((select name_project from Projects 
+					WHERE name_project not in (Select name_task from Tasks)
+						),
 						'description',
 						0,
 						NULL,
-						(select SCOPE_IDENTITY() from Projects))			
+						(select SCOPE_IDENTITY() from Projects))
+			END			
 	END
 ELSE
 	BEGIN
